be6cd9c4815beed506a1836a741d6277142d0856
Rahul P S <rahulps995@gmail.com>
2023-11-23 13:34:47 +0530

    created basic native app

---
+++ App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {View, Text, SafeAreaView} from 'react-native';
+
+function App() {
+  return (
+    <>
+      <SafeAreaView>
+        <View>
+          <Text style={{fontSize: 20}}>Hello World!</Text>
+        </View>
+      </SafeAreaView>
+    </>
+  );
+}
+
+export default App;
